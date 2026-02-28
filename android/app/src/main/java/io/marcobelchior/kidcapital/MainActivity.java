70a1ad920398526e54af850e97f03f91bd23c0c7
marc bernard <marc.bernard@ece-france.com>
2026-02-28 10:16:30 +0100

    build: init capacitor and android platform

---
+++ android/app/src/main/java/io/marcobelchior/kidcapital/MainActivity.java
@@ -0,0 +1,5 @@
+package io.marcobelchior.kidcapital;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.marcobelchior.kidcapital',
+  appName: 'KidCapital',
+  webDir: 'dist'
+};
+
+export default config;
