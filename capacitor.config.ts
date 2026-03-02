40bf08f186be13fd6013b943e0a62dc897475708
marc bernard <marc.bernard@ece-france.com>
2026-03-02 18:42:10 +0100

    fix: resolve mismatch between App ID and Team ID using dynamic Profile parsing

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.marcobelchior.kidcapital',
+  appId: 'com.marcobloch.kidcapital',
   appName: 'KidCapital',
   webDir: 'dist'
 };
